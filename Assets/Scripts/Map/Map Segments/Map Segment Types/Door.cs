787524449055dd392dc7429f0144390766ea45de
ChickenChamp71 <gabrielandersondev@gmail.com>
2025-04-10 00:51:32 -0700

    swap back to laptop

--- Assets/Scripts/Map/Map Segments/Map Segment Types/Door.cs
+++ Assets/Scripts/Map/Map Segments/Map Segment Types/Door.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "base_doorNode", menuName = "Nodes/Map/DoorNode")]
-public class DoorData : MapNodeData
+public class Door : MapSegment
 {
+    public bool isElectric;
     public bool isWorking;
     public bool isOpen;
 }
