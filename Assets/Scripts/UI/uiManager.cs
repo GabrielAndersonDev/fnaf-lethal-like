9e2fd3c6d45020ca38d53c5a986385a8e37cf8be
GabrielAndersonDev <gabrielandersondev@gmail.com>
2025-04-24 20:09:12 -0700

    may be the worlds ugliest UI but it is certainly there

---
+++ Assets/Scripts/UI/uiManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class uiManager : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+
+    }
+
+    public void LoadMainMenu()
+    {
+        
+    }
+}
