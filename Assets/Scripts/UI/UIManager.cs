2fddd900362495fbf2f91251d55138e49e5c08b3
ChickenChamp71 <gabrielandersondev@gmail.com>
2025-05-13 01:36:27 -0700

    pause menu basics working, adding button implementation plus quit confirm popup

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,90 @@ using UnityEngine.UIElements;
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject pauseMenu;
+    GameObject pauseObject;
+    VisualElement pauseUi;
     [SerializeField]
-    GameObject baseUI; 
-    void Start()
+    GameObject guiObject;
+    VisualElement GUI; 
+
+    public static UIManager Instance { get; private set; }
+    
+    public bool isPaused = false;
+    bool isPopup = false;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        pauseUi = pauseObject.GetComponent<UIDocument>().rootVisualElement;
+        GUI = guiObject.GetComponent<UIDocument>().rootVisualElement;
+
+        Button resumeBtn = pauseUi.Q<Button>("resume-btn");
+        Button settingsBtn = pauseUi.Q<Button>("resume-btn");
+        Button mainReturnBtn = pauseUi.Q<Button>("main-return-btn");
+        Button quitBtn = pauseUi.Q<Button>("quit-btn");
+        Button popupConfirmBtn = pauseUi.Q<Button>("popup-confirm-btn");
+        Button popupCancelBtn = pauseUi.Q<Button>("popup-cancel-btn");
+
+        if (isPaused)
+        {
+            pauseUi.SetEnabled(true);
+            pauseUi.visible = true;
+        }
+        else
+        {
+            pauseUi.SetEnabled(false);
+            pauseUi.visible = false;
+        }
+
+        resumeBtn.clicked += ResumeBtnClicked;
+        settingsBtn.clicked += SettingsBtnClicked;
+        mainReturnBtn.clicked += MainReturnBtnClicked;
+        quitBtn.clicked += QuitBtnClicked;
+    }
+    
+    public bool TogglePause()
+    {
+        if (isPaused)
+        {
+            pauseUi.SetEnabled(false);
+            isPaused = false;
+            pauseUi.visible = false;
+        }
+        else
+        {
+            pauseUi.SetEnabled(true);
+            isPaused = true;
+            pauseUi.visible = true;
+        }
+        return isPaused;
+    }
+
+    private void ResumeBtnClicked()
+    {
+        TogglePause();
+    }
+
+    private void SettingsBtnClicked()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MainReturnBtnClicked()
     {
-        
+
+    }
+
+    private void QuitBtnClicked()
+    {
+
     }
 }
