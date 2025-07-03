9a1d3644a6f045dad5e46829d2ebdd92dc52c947
GabrielAndersonDev <gabrielandersondev@gmail.com>
2025-07-02 20:23:31 -0700

    switching to main desktop

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public partial class Enemy : MonoBehaviour
+public class Enemy : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Enemy Info")]
+    public string enemyName;
+    public int enemyID;
+    public EnemyData enemyData;
+    public Team team;
+    public bool isDeactivated;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Enemy Spawning")]
+    public RoomType spawnRoom;
+
+    [Header("Enemy AI")]
+    [SerializeField]
+    EnemyAI enemyAI;
+
+    public virtual void InitializeEnemy(EnemyData data)
     {
-        
+        enemyData = data;
+
+        if (enemyData != null)
+        {
+            enemyName = data.enemyName;
+            data.enemyID = enemyID;
+            team = data.team;
+            isDeactivated = data.isDeactivated;
+        }
+        else
+        {
+            Debug.LogError("EnemyData is null. Cannot initialize enemy.");
+            return;
+        }
     }
 }
---
+++ Assets/Scripts/Enemies/EnemyClasses/MovingEnemy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MovingEnemy : Enemy
+{
+    [Header("Enemy Stats")]
+    public int baseSpeed;
+    public int baseStamina;
+
+    [Header("Enemy Movement AI")]
+    int tempAI = 1;
+
+
+}
