2d1579ced85c9acea1cbcf0aeabd0e8254833826
ChickenChamp71 <gabrielandersondev@gmail.com>
2025-08-18 18:28:50 -0700

    added enemymovementdata, moving to laptop

--- Assets/Scripts/Enemies/EnemyClasses/MovingEnemy.cs
+++ Assets/Scripts/Enemies/EnemyClasses/MovingEnemy.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
 
+public enum EnemyState
+{
+    Invalid = -2,
+    None = -1,
+    First,
+    Search = First,
+    Pursuit,
+    Max
+}
+
 public class MovingEnemy : Enemy
 {
+    bool isStunned;
+
     [Header("Enemy Stats")]
     public int baseSpeed;
     public int baseStamina;
 
     [Header("Enemy Movement AI")]
-    int tempAI = 1;
+    [SerializeField]
+    EnemyMovementData movementAi;
+    EnemyState enemyState;
 
 
 }
