22ff69808b9e6aec328f52a30e97c76ce39f475a
ChickenChamp71 <gabrielandersondev@gmail.com>
2025-09-10 02:24:33 -0700

    switching to laptop

--- Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -10,6 +10,11 @@ using UnityEngine.TextCore;
 public class MainMenuManager : MonoBehaviour
 {
     VisualElement uiDoc;
+
+    Box startBox;
+    Box saveSlotContainer;
+
+    bool isHostClicked = false;
     
     private void Start()
     {
@@ -21,13 +26,37 @@ public class MainMenuManager : MonoBehaviour
             DontDestroyOnLoad(NetworkScript.Singleton);
             uiDoc = GetComponent<UIDocument>().rootVisualElement;
 
+            startBox = uiDoc.Q<Box>("start-box");
+
             Button hostBtn = uiDoc.Q<Button>("host-btn");
             Button clientBtn = uiDoc.Q<Button>("client-btn");
             Button serverBtn = uiDoc.Q<Button>("server-btn");
 
+            saveSlotContainer = uiDoc.Q<Box>("save-slot-container");
+
+            Button slotZero = uiDoc.Q<Button>("slot-0");
+            Button slotOne = uiDoc.Q<Button>("slot-1");
+            Button slotTwo = uiDoc.Q<Button>("slot-2");
+            Button slotThree = uiDoc.Q<Button>("slot-3");
+
+            Button backBtn = uiDoc.Q<Button>("back-btn");
+
+            if (!isHostClicked)
+            {
+                startBox.style.display = DisplayStyle.Flex;
+                saveSlotContainer.style.display = DisplayStyle.None;
+            }
+            else
+            {
+                startBox.style.display = DisplayStyle.None;
+                saveSlotContainer.style.display = DisplayStyle.Flex;
+            }
+
             hostBtn.clicked += OnHostClicked;
             clientBtn.clicked += OnClientClicked;
             serverBtn.clicked += OnServerClicked;
+
+            slotZero.clicked += OnSlotClicked(0);
         }
         else
         {
@@ -37,6 +66,22 @@ public class MainMenuManager : MonoBehaviour
     }
 
     public void OnHostClicked()
+    {
+        isHostClicked = true;
+
+        if (!isHostClicked)
+        {
+            startBox.style.display = DisplayStyle.Flex;
+            saveSlotContainer.style.display = DisplayStyle.None;
+        }
+        else
+        {
+            startBox.style.display = DisplayStyle.None;
+            saveSlotContainer.style.display = DisplayStyle.Flex;
+        }
+    }
+
+    void OnSlotClicked(int slot)
     {
         NetworkScript.Singleton.LoadHostGame();
     }
